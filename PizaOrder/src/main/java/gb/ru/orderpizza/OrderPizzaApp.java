3cf99b05cd57ac9ef73d17834b004e014e34aa18
Nadir <akh_nodir@mail.ru>
2025-02-24 11:24:29 +0500

    Entry point. Program class

---
+++ PizaOrder/src/main/java/gb/ru/orderpizza/OrderPizzaApp.java
@@ -0,0 +1,21 @@
+package gb.ru.orderpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Books library application.
+ */
+@SpringBootApplication
+public class OrderPizzaApp {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+		SpringApplication.run(OrderPizzaApp.class, args);
+	}
+
+}
