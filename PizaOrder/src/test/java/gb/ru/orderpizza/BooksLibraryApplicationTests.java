60f7621cfa85ad04d85b0d7eff8982fb473cc895
Nadir <akh_nodir@mail.ru>
2025-02-27 16:39:26 +0500

    Changes

---
+++ PizaOrder/src/test/java/gb/ru/orderpizza/BooksLibraryApplicationTests.java
@@ -0,0 +1,19 @@
+package gb.ru.orderpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * The type Books library application tests.
+ */
+@SpringBootTest
+class BooksLibraryApplicationTests {
+
+    /**
+     * Context loads.
+     */
+    @Test
+	void contextLoads() {
+	}
+
+}
